813b43d22a7952b0d09a2e59011013a9cb3fa48f
Edgars Zagorskis <edgars.zagorskis@gmail.com>
2026-03-04 17:22:16 +0200

    fix: update default server URL to port 39999

--- src/Sirr.Client/SirrOptions.cs
+++ src/Sirr.Client/SirrOptions.cs
@@ -6,9 +6,9 @@ namespace Sirr;
 public sealed class SirrOptions
 {
     /// <summary>
-    /// Base URL of the Sirr server. Defaults to <c>http://localhost:8080</c>.
+    /// Base URL of the Sirr server. Defaults to <c>http://localhost:39999</c>.
     /// </summary>
-    public string Server { get; set; } = "http://localhost:8080";
+    public string Server { get; set; } = "http://localhost:39999";
 
     /// <summary>
     /// Bearer token for authentication.
